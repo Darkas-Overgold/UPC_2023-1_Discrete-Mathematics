0765801d813b3c94b4c0ddc6f4411ff376f3c859
Darkas Voidtear <105468040+Darkas-Voidtear@users.noreply.github.com>
2023-05-06 15:42:46 -0500

    Add files via upload

---
+++ Menú.cpp
@@ -0,0 +1,7 @@
+#include "Menú.h"
+using namespace System;
+using namespace System::Windows::Forms;
+void main() {
+	Application::EnableVisualStyles();
+	Application::Run(gcnew(TB_Grupo5_SI38::Menú));
+}
